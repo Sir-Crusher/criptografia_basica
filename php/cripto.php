eba333c0006ea081cacc61f14789a9addfcd8d77
Sir-Crusher <abnermorato@gmail.com>
2020-06-06 15:17:21 -0300

    Create cripto.php
    classe para métodos de criptografia

---
+++ php/cripto.php
@@ -0,0 +1,46 @@
+<?php 
+class Cripto
+
+{
+
+    function encriptar ($chave, $texto)
+    {
+        $tamanho_texto = $texto->strlen();
+        $i = 0;
+        
+        while ($chave->strlen() <> $texto->strlen())
+        {
+            //estender chave até o tamanho do texto
+            
+            if ($tamanho_texto == $i)
+                $i = 0;
+            
+            $chave +=($chave-> substr($i, 1));
+            
+            $i++;
+        }
+        
+        //encriptar o texto
+        
+        $texto_encriptado = "";
+        
+        for ($i = 0; i < $tamanho_texto; $i++)
+        {
+            /*conseguir a posição do caractere em código ASCII,
+            somar com a posição relativa do caractere à chave e realizar o módulo 26 */
+            
+            $caractere_encriptado = ($texto->substr($i, 1) + $chave->substr($i, 1)) %26;
+            
+            //Converter a posição resultante em ASCII para caractere
+            
+            $caractere_encriptado += 'A';
+            
+            //Adicionar o caractere encriptado ao texto final
+            
+            $texto_encriptado += (string)($caractere_encriptado);
+        }
+        
+        return $texto_encriptado;
+    }
+}
+?>
