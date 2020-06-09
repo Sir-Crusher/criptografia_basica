effb480ffa7d6c9775beeafea5faf8733b8823f3
Sir-Crusher <abnermorato@gmail.com>
2020-06-09 10:21:34 -0300

    Update sair.php

--- php/sair.php
+++ php/sair.php
@@ -4,4 +4,4 @@ session_start();
 unset($_SESSION['id'], $_SESSION['nome'], $_SESSION['email']);
 
 $_SESSION['msg'] = "Deslogado com sucesso";
-header("Location: https://serverc.sytes.net/index.html");
+header("Location: ./index.html");
